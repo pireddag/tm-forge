4c519c2e9741e669d6a77447feb5cdcd8d3f572b
pireddag <pireddag@gmail.com>
2024-01-20 13:34:31 +0100

    Clean up csv-table-init

--- miscellanea/csv-table/csv-table/progs/csv-table-init.scm
+++ miscellanea/csv-table/csv-table/progs/csv-table-init.scm
@@ -5,35 +5,38 @@
 ;; Mode definitions
 ;; See Scheme developer guide, p. 28
 (texmacs-modes
- (in-big-table% (inside? 'big-table) in-text%))
+  (in-big-table% (inside? 'big-table) in-text%))
 
 
-;; With this Widget we issue a message to the user when the function is used outside
+;; With this widget we issue a message to the user when the function is used outside
 ;; the planned environments
 (tm-widget (csv-table-message-widget cmd)
-	   (vlist
-	    (centered
-	     (text "Please insert table in plain text")
-	     ===
-	     (text "or big-table environment")))
-	   (bottom-buttons >> ("Ok" (cmd "Ok"))))
+  (vlist
+    (centered
+      (text "Please insert table in plain text")
+      ===
+      (text "or big-table environment")))
+  (bottom-buttons >> ("Ok" (cmd "Ok"))))
 
-
-
-
+;; Column separators
 
 (tm-define csv-table:separator  #\,)
 
 (tm-define csv-table:separator-list
-  `(("," . ,#\,) (";" . ,#\;) ("tab" . ,#\tab) ("space" . ,#\space)))
+  `(("," . ,#\,)
+    (";" . ,#\;)
+    ("tab" . ,#\tab)
+    ("space" . ,#\space)))
 
 
 (tm-widget (set-separator)
   (resize "200px" "100px"
     (scrollable
-      (choice (set! csv-table:separator (cdr (assoc answer csv-table:separator-list)))
-	      '("," ";" "tab" "space")
-	      ","))))
+      (choice
+       (set! csv-table:separator
+	     (cdr (assoc answer csv-table:separator-list)))
+       '("," ";" "tab" "space")
+       ","))))
 
 ;; Need a widget to launch the reading from the file once that a choice has
 ;; been made
@@ -46,25 +49,33 @@
 ;; all of the environments where it is expected to work, or to the insertion of
 ;; a table at the cursor position
 
-(tm-define (csv-table-condition) (and  (or (in-text?) (in-big-table?)) (not (in-table?))))
-
-
+(tm-define (csv-table-condition)
+  (and
+   (or (in-text?) (in-big-table?))
+   (not (in-table?))))
 
 (kbd-map
- ("t a b l e tab"
-  (dialogue-window csv-table-message-widget (lambda (arg) (noop)) "Insert table message")))
-
+  ("t a b l e tab"
+   (dialogue-window
+    csv-table-message-widget
+    (lambda (arg) (noop))
+    "Insert table message")))
 
 (kbd-map
   (:require (csv-table-condition))
-  ("t a b l e tab" (choose-file insert-csv-table "choose table file" "")))
-
-(lazy-define-force insert-table-menu)
+  ("t a b l e tab"
+   (choose-file
+    insert-csv-table
+    "choose table file"
+    "")))
 
 ;; Set menu item, inactive when the table cannot be inserted
+
+(lazy-define-force insert-table-menu)
 
 (tm-menu (insert-table-menu)
   (former)
   ---
   (when (csv-table-condition)
-    ("Insert table from csv file" (choose-file insert-csv-table "choose table file" ""))))
+    ("Insert table from csv file"
+     (choose-file insert-csv-table "choose table file" ""))))
